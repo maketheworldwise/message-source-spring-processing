4cbb91f84045b80d781ae0780498e907ef9da0c5
Kevin <maketheworldwise@gmail.com>
2022-05-29 14:52:30 +0900

    :white_check_mark: Accept-Language 헤더 방식으로 테스트 코드 수정

--- src/test/java/com/example/application/api/controller/MessageSourceControllerTest.java
+++ src/test/java/com/example/application/api/controller/MessageSourceControllerTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -18,22 +19,22 @@ class MessageSourceControllerTest {
 	private MockMvc mockMvc;
 
 	@Test
-	@DisplayName("[GET] /say/hello?lang=ko-kr")
+	@DisplayName("Accept-Language: ko-kr, [GET] /say/hello")
 	public void sayHelloInKo() throws Exception {
 		mockMvc.perform(MockMvcRequestBuilders.get("/say/hello")
-				.contentType(MediaType.APPLICATION_JSON)
-				.param("lang", "ko-kr"))
+				.header(HttpHeaders.ACCEPT_LANGUAGE, "ko-kr")
+				.contentType(MediaType.APPLICATION_JSON))
 			.andExpect(status().isOk())
 			.andExpect(content().string("안녕!"))
 			.andDo(print());
 	}
 
 	@Test
-	@DisplayName("[GET] /say/hello?lang=en-us")
+	@DisplayName("Accept-Language: en-us, [GET] /say/hello")
 	public void sayHelloInEn() throws Exception {
 		mockMvc.perform(MockMvcRequestBuilders.get("/say/hello")
-				.contentType(MediaType.APPLICATION_JSON)
-				.param("lang", "en-us"))
+				.header(HttpHeaders.ACCEPT_LANGUAGE, "en-us")
+				.contentType(MediaType.APPLICATION_JSON))
 			.andExpect(status().isOk())
 			.andExpect(content().string("hi!"))
 			.andDo(print());
